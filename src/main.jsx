6261da8d342f5f6121324c7971f046c25cf038ee
vadim <vadimlevadko@gmail.com>
2025-01-12 21:06:42 +0200

    chore: minor updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/js/index.esm.js';
+
+import App from './app/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
