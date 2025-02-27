e11d923c67ba63956fae7ec057015591b003490e
vadim <vadimlevadko@gmail.com>
2025-02-27 22:27:31 +0200

    new structure for project

--- web/src/page.jsx
+++ web/src/page.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 
-import MainSection from './src/app/components/MainSection'
+import MainSection from './app/components/MainSection/index.js'
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/js/index.esm.js';
