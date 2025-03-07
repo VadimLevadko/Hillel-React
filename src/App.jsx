76206d8bb3e3535736db432839a6e4ca2e8d7169
vadim <vadimlevadko@gmail.com>
2025-03-07 15:33:39 +0200

    feat: change structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { createRoot } from 'react-dom/client'
 
-import MainSection from './app/components/MainSection/index.js'
-
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/js/index.esm.js';
+import MainSection from './app/components/MainSection'
 
 createRoot(document.getElementById('root')).render(
     <main className="container">
