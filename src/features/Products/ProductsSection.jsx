a67de1dfc86c66512b573241c0f11d597424de41
vadim <vadimlevadko@gmail.com>
2025-03-11 17:03:47 +0200

    feat: add loading for main products list

--- src/features/Products/ProductsSection.jsx
+++ src/features/Products/ProductsSection.jsx
@@ -2,20 +2,21 @@ import { useSelector } from 'react-redux';
 import { selectVisibleProducts } from "@features/Filter/filter-slice.jsx";
 
 import ProductItem from './ProductItem.jsx'
+import Loading from "@components/Loading"
 
 export default function ProductsSection() {
     const products = useSelector(selectVisibleProducts);
 
+    if(!products.length) return <Loading />;
+
     return (
         <section className="products-section">
             <div className="max-w-full py-[10px] px-[20px]">
-                {!!products.length && (
-                    <div className="grid grid-cols-5 gap-4">
-                        {products.map((product) => {
-                            return <ProductItem content={product} key={product.id} />
-                        })}
-                    </div>
-                )}
+                <div className="grid grid-cols-5 gap-4">
+                    {products.map((product) => {
+                        return <ProductItem content={product} key={product.id}/>
+                    })}
+                </div>
             </div>
         </section>
     )
